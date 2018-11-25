35b8c4adb87dde8cf95f9afd019870b3e10fb5c9
Chernolichenko Alexey <chernolichenko.ai@gmail.com>
2018-11-25 12:45:53 +0300

    add chenges for warnings ESlint

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
-window.renderStatistics = function(ctx, names, times){
-  //считаем максимальное время
+window.renderStatistics = function (ctx, names, times) {
+  // считаем максимальное время
   var maxTime = calculateMaxTime(times);
 
   // рисуем белое облако с тенью
@@ -14,12 +14,12 @@ window.renderStatistics = function(ctx, names, times){
   createText(ctx, '16px PT Mono', '#000', 'Список результатов:', 120, 50);
 
   // перебираем всех игроков и для каждого вызываем функцию рисующую его колонку с его именем и значением времени
-  for (var i = 0; i < names.length; i++){
+  for (var i = 0; i < names.length; i++) {
     createPlayerColumn(ctx, names[i], times[i], i, maxTime);
   }
 };
 
-var createPlayerColumn = function(ctx, playerName, playerTime, playerNumber, maxTime){
+var createPlayerColumn = function (ctx, playerName, playerTime, playerNumber, maxTime) {
 
   var columnMaxHeiht = 150;
   var columnWidth = 40;
@@ -37,76 +37,73 @@ var createPlayerColumn = function(ctx, playerName, playerTime, playerNumber, max
   createText(ctx, '16px PT Mono', '#000', playerName, columnOffsetX, 270);
 };
 
-var createText = function(ctx, font, color, text, offsetX, offsetY){
+var createText = function (ctx, font, color, text, offsetX, offsetY) {
   ctx.font = font;
   ctx.fillStyle = color;
   ctx.fillText(text, offsetX, offsetY);
 };
 
-var createShadow = function(ctx, offsetX, offsetY, color){
+var createShadow = function (ctx, offsetX, offsetY, color) {
 
-  if(!color){
-  ctx.shadowColor = '#fff';
-  }
-  else{
+  if (!color) {
+    ctx.shadowColor = '#fff';
+  } else {
     ctx.shadowColor = color;
   }
   ctx.shadowOffsetX = offsetX;
   ctx.shadowOffsetY = offsetY;
 };
 
-var createRect = function(ctx, color, offsetX, offsetY, width, height){
+var createRect = function (ctx, color, offsetX, offsetY, width, height) {
   ctx.fillStyle = color;
   ctx.fillRect(offsetX, offsetY, width, height);
 };
 
-var createColumnColor = function(playerName, columnOpacity){
+var createColumnColor = function (playerName, columnOpacity) {
   var currentColor = '';
-  if(playerName === "Вы"){
+  if (playerName === 'Вы') {
     currentColor = 'rgba(255, 0, 0, 1)';
-  }
-
-  else{
+  } else {
     currentColor = 'rgba(0, 34, 255,' + columnOpacity + ' )';
   }
 
   return currentColor;
 };
 
-var calculateMaxTime = function(times){
+var calculateMaxTime = function (times) {
   var currentTime = 0;
-  for (var i = 0; i < times.length; i++){
+  for (var i = 0; i < times.length; i++) {
 
-    if(currentTime < times[i]){
+    if (currentTime < times[i]) {
 
       currentTime = times[i];
     }
   }
-  return  currentTime;
+  return currentTime;
 };
 
-var calculateColumnHeight = function(columnMaxHeiht, maxPlayersTime, playerTime){
+var calculateColumnHeight = function (columnMaxHeiht, maxPlayersTime, playerTime) {
   var columnHeightPlayer = 0;
   var changeDirection = -1;
   columnHeightPlayer = playerTime * columnMaxHeiht / maxPlayersTime;
 
-  return  Math.round(columnHeightPlayer) * changeDirection;
+  return Math.round(columnHeightPlayer) * changeDirection;
 };
 
-var createColumnOpacity = function(playerNumber){
+var createColumnOpacity = function (playerNumber) {
   var minOpacity = 0.5;
   var playerColorOpacity = playerNumber / 10 + minOpacity;
   return playerColorOpacity;
 };
 
-var calculateOffsetX = function(columnNumber, columnWidth, columnMargin){
+var calculateOffsetX = function (columnNumber, columnWidth, columnMargin) {
   var defaultOffsetX = 120;
 
-  if(columnNumber === 0){
+  if (columnNumber === 0) {
     return defaultOffsetX;
   }
 
-  return defaultOffsetX + (columnWidth  + columnMargin) * columnNumber;;
+  return defaultOffsetX + (columnWidth + columnMargin) * columnNumber;
 };
 
 
